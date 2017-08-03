78bab5c58eef4feeeed67eace913fcb0a2b3b573
ismailcanerkal <244087@ogr.ktu.edu.tr>
2017-08-03 06:03:37 +0300

    Add by GUI

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class HelloWorld{
+  JFrame frame;
+
+  HelloWorld(){
+    frame = new JFrame("Hello World!");
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300,300);
+    frame.setVisible(true);
+  }
+
   public static void main(String[] args){
-    System.out.println("Hello World!");
+    SwingUtilities.invokeLater(new Runnable(){
+      public void run(){
+        new HelloWorld();
+      }
+    });
   }
 }
